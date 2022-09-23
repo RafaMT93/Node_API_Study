69363eabb327c5c56de19452e1346c35a2de4201
Rafael Tiburcio <rafat93.rdm@outlook.com.br>
2022-09-23 20:47:10 -0300

    Fix put method

--- src/moviesResolvers/put.js
+++ src/moviesResolvers/put.js
@@ -2,16 +2,16 @@ const fs = require('fs');
 
 const putMovies = {
     putMovie: (req, res, movies) => {
-        const { id } = req.params;
         const { name, photo, description, casting } = req.body;
+        const { id } = req.params;
         const movie = { id, name, photo, description, casting };
-        const movieIndex = movies.findIndex(movie => movie.id === id);
+        const movieIndex = movies.find(movie => movie.id === id);
         if(movieIndex < 0) return res.status(400).json("Movie not found!");
-        movie.id = id;
-        movie.name = name ? name : movie.name;
-        movie.photo = photo ? photo : movie.photo;
-        movie.description = description ? description : movie.description;
-        movie.casting = casting ? casting : movie.casting;
+        movieIndex.id = id;
+        movieIndex.name = name ? name : movieIndex.name;
+        movieIndex.photo = photo ? photo : movieIndex.photo;
+        movieIndex.description = description ? description : movieIndex.description;
+        movieIndex.casting = casting ? casting : movieIndex.casting;
         fs.writeFile('./src/data/movies.json', JSON.stringify(movies), err => {
             if(err) throw err;
             res.status(200).send(movies);
