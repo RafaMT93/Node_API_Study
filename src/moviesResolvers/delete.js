cacc65f06f45425c0fb584bf69775da3957511d9
Rafael Tiburcio <rafat93.rdm@outlook.com.br>
2022-09-23 19:53:09 -0300

    fix delete method

--- src/moviesResolvers/delete.js
+++ src/moviesResolvers/delete.js
@@ -5,6 +5,7 @@ const deleteMovie = {
         const { id } = req.params;
         const movieIndex = movies.findIndex(movie => movie.id === id);
         if(movieIndex < 0) return res.status(404).json("Movie not found!");
+        movies.splice(movieIndex, 1);
         fs.writeFile('./src/data/movies.json', JSON.stringify(movies), err => {
             if(err) throw err;
             res.status(204).send("Movie deleted!");
